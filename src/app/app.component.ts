f98d2fe73c75e3f1ba70e9978186ec6738460a9e
Ismael-V <ismaelpsabi@gmail.com>
2025-05-20 17:20:22 +0200

    Modificado pagina principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LogoNavbarComponent } from './logo-navbar/logo-navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LogoNavbarComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/logo-navbar/logo-navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-navbar',
+  imports: [],
+  templateUrl: './logo-navbar.component.html',
+  styleUrl: './logo-navbar.component.css'
+})
+export class LogoNavbarComponent {
+
+}
